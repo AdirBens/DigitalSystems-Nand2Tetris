d6486ad6c7da7a719108cfa2d719ffa603902585
Adir Ben Shimol <adir.bens@gmail.com>
2022-11-20 13:35:44 +0200

    Implement Fill.asm function v1

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,12 +3,49 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input.
+// Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(SETINGS) // Sets i to hold the address of end of screen
+    @SCREEN
+    D=A
+    @8192
+    D=D+A
+    @i
+    M=D
+(KBD-LOOP)  // Check for user input
+    @i
+    D=M
+    @SCREEN
+    D=D-M
+    @SETINGS
+    D;JLE
+
+    @KBD
+    D=M
+    @SET-BLACK
+    D;JGT
+    @SET-WHITE
+    D;JEQ
+
+(SET-BLACK)
+    @i
+    A=M
+    M=-1
+    @i
+    M=M-1
+    @KBD-LOOP
+    0;JMP
+
+(SET-WHITE)
+    @i
+    A=M
+    M=0
+    @i
+    M=M-1
+    @KBD-LOOP
+    0;JMP
+
+// (*) idle logic?
