1e4c4a80a85f7015ca101d421df4a04650520a9f
Adir Ben Shimol <adir.bens@gmail.com>
2022-12-08 01:57:36 +0200

    Add SymbolTable class API

--- 06/HackAssembler.java
+++
@@ -1,9 +0,0 @@
-public class HackAssembler {
-/**
- * File Names By convention, programs in binary machine code and in assembly
- * code are stored in text files with ‘‘hack’’ and ‘‘asm’’ extensions, respectively. Thus, a
- * Prog.asm file is translated by the assembler into a Prog.hack file.
- */
-
-
-}
--- 06/SymbolTable.java
+++ 06/SymbolTable.java
@@ -1,19 +1,16 @@
-import java.security.InvalidKeyException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
+
+/**
+ * Hack's SymbolTable
+ * create and maintain the correspondence between symbols and their meaning.
+ * particularly resolves Symbol into its actual Address in Hack's RAM and ROM spaces.
+ */
 public class SymbolTable {
-    protected Map<String, Integer> symbols;
-    protected Pattern validSymbolPattern;
+    private Map<String, Integer> symbols;
 
-    public SymbolTable() throws PatternSyntaxException {
-        // TODO: consider variable-symbols, label-symbols, predefine-symbols and user-defined symbols
-        this.symbols = new HashMap<>();
-        loadPreDefineSymbols();
-        this.validSymbolPattern = Pattern.compile("^(\\p{Alpha}|\\$|\\.|:|_)+(\\p{Alnum}|\\$|\\.|:|_)*");
+
+    public SymbolTable(){
     }
 
     /**
@@ -23,14 +20,7 @@ public class SymbolTable {
      * @param symbol
      * @param address
      */
-    public void addEntry(String symbol, int address) throws InvalidKeyException {
-        Matcher matcher = validSymbolPattern.matcher(symbol);
-        if (matcher.find()) {
-            symbols.put(symbol, address);
-        } else {
-            throw new InvalidKeyException("Invalid symbol: A user-defined symbol can be any sequence of letters/" +
-                    ", digits, underscore, dot, dollar sign, and colon, that does not begin with a digit");
-        }
+    public void addEntry(String symbol, int address){
     }
 
     /**
@@ -39,46 +29,41 @@ public class SymbolTable {
      * @return
      */
     public boolean contains(String symbol){
-        return symbols.containsKey(symbol);
+        return true;
     }
 
     /**
-     * Returns the address associated with the symbol
+     * return the RAM address which symbol is mapped to in decimal form
      * @param symbol
-     * @return the address of given symbol or null if table contains no mapping for the symbol
+     * @return decimal RAM address if symbol exists in table, else -1
      */
-    public int GetAddress(String symbol){
-        return symbols.get(symbol);
+    public int getDecimalAddress(String symbol){
+        return 0;
+    }
+
+    /**
+     * return the RAM address which symbol is mapped to in Binary form
+     * @param symbol
+     * @return 16Bits Binary RAM address if symbol exists in table, else ""
+     */
+    public String getBinaryAddress(String symbol){
+        return "";
     }
 
     /**
      * Any Hack assembly program is allowed to use the following predefined symbols.
-     *  Note that each one of the top five RAM locations can be referred to using two predefined symbols.
-     *  For example, either R2 or ARG can be used to refer to RAM[2].
+     *  (!) Note that each one of the top five RAM locations can be referred to using two predefined symbols.
+     *      For example, either R2 or ARG can be used to refer to RAM[2].
      */
-    private void loadPreDefineSymbols(){
+    private void loadPreDefinedSymbols(){
         symbols.put("SP", 0);
         symbols.put("LCL", 1);
         symbols.put("ARG", 2);
         symbols.put("THIS", 3);
         symbols.put("THAT", 4);
-        symbols.put("R0", 0);
-        symbols.put("R1", 1);
-        symbols.put("R2", 2);
-        symbols.put("R3", 3);
-        symbols.put("R4", 4);
-        symbols.put("R5", 5);
-        symbols.put("R6", 6);
-        symbols.put("R7", 7);
-        symbols.put("R8", 8);
-        symbols.put("R9", 9);
-        symbols.put("R10", 10);
-        symbols.put("R11", 11);
-        symbols.put("R12", 12);
-        symbols.put("R13", 13);
-        symbols.put("R14", 14);
-        symbols.put("R15", 15);
         symbols.put("SCREEN", 16384);
         symbols.put("KBD", 24576);
+
+        for (int i = 0; i < 16; i++) symbols.put("R"+i, i);
     }
 }
