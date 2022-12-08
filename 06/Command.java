d647ef5728349da6dd3c6933de2ae64b4498e040
Adir Ben Shimol <adir.bens@gmail.com>
2022-12-08 02:04:18 +0200

    Add and Implements Command enum - holds all available Hack Command and Instruction types and their pattern

---
+++ 06/Command.java
@@ -0,0 +1,20 @@
+import java.util.regex.Pattern;
+
+/**
+ * Command types and their valid patterns
+ * <p>
+ * Be aware of the fact that L pattern is private case of C Pattern -
+ * (!)(!)(!) DO NOT CHANGE Command Enum INNER ORDER (!)(!)(!)
+ */
+enum Command{
+    EMPTY(Pattern.compile("^$")),
+    COMMENT(Pattern.compile("(^//.*$)")),
+    L_COMMAND(Pattern.compile("^\\((.*)\\)$")),
+    A_COMMAND(Pattern.compile("^@(.*)$")),
+    C_COMMAND(Pattern.compile("((^[ADM]{0,3})=)?([01ADM!-]?[-+&|]?[01ADM])(;(J[EGLMN][TQEP]$))?"));
+
+    public Pattern pattern;
+    Command(Pattern commandPattern){
+        pattern = commandPattern;
+    }
+}
