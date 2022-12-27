f83665e6ec0cb5669aa3e6ad9e478ce4c55fe692
Adir Ben Shimol <adir.bens@gmail.com>
2022-12-27 16:17:03 +0200

    Change ASM base return

--- 08/VMTranslator/CodeWriter.py
+++ 08/VMTranslator/CodeWriter.py
@@ -74,7 +74,7 @@ class CodeWriter(object):
         Args: label (str) - string represents the label
         Returns: None.
         """
-        label = "{}${}".format(self._in_function[-1], command.arg1) if len(self._in_function) else command.arg1
+        label = "{}${}".format(self._in_function[-1], command.arg1) if len(self._in_function) else "{}".format(command.arg1)
         cmd_str = self._templates[command.operation][command.command_type].format(label=label)
         self._output_file.write(self._comment_code_block(command, cmd_str))
         self._asm_lines_written += cmd_str.count("\n")
@@ -86,7 +86,7 @@ class CodeWriter(object):
         Returns: None.
         """
 
-        label = "{}${}".format(self._in_function[-1], command.arg1) if len(self._in_function) else command.arg1
+        label = "{}${}".format(self._in_function[-1], command.arg1) if len(self._in_function) else "{}".format(command.arg1)
         cmd_str = self._templates[command.operation][command.command_type].format(dest=label)
         self._output_file.write(self._comment_code_block(command, cmd_str))
         self._asm_lines_written += cmd_str.count("\n")
@@ -135,8 +135,10 @@ class CodeWriter(object):
         :param command:
         """
         self._in_function.append(command.arg1)
-        cmd_str = self._templates[command.operation][command.command_type].format(function_name=command.arg1)
-        cmd_str += int(command.arg2) * (self._asm.PushD)
+        cmd_str = self._templates[command.operation][command.command_type].format(function_name=command.arg1,
+                                                                                  label_counter=self._label_counter)
+        self._label_counter += 1
+        cmd_str += int(command.arg2) * (self._asm.PushD + "\n")
         self._output_file.write(self._comment_code_block(command, cmd_str))
         self._asm_lines_written += cmd_str.count("\n")
 
--- 08/VMTranslator/VMTranslator.py
+++ 08/VMTranslator/VMTranslator.py
@@ -16,6 +16,7 @@ class VMTranslator(object):
         self.vm_files = self._load_program_path(program_path)
         if os.path.isdir(program_path):
             self._base_name = os.path.join(program_path, os.path.basename(program_path)).split('.')[0] + '.asm'
+            self._base_name = f'{program_path}/{program_path.split("/")[-1]}.asm'
         else:
             self._base_name = program_path
         self.parser = None
@@ -26,7 +27,7 @@ class VMTranslator(object):
         Orchestrate Translation VM Commands to HackAssembly process.
         Returns: None.
         """
-        self.code_writer.write_init()
+        # self.code_writer.write_init()
         for vm_file in self.vm_files:
             self._set_file_to_modules(vm_file)
             while self.parser.has_more_commands():
